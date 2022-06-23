9d0610e827d5c949bd3d4f9ff858bb97f9ee86da
nicoli <nicolas@ovrsea.com>
2022-06-23 12:30:01 +0200

    feat: implement monad

--- src/waiter/domain/orderDrink.ts
+++ src/waiter/domain/orderDrink.ts
@@ -1,9 +1,10 @@
 import { checkEnoughMoneyWasInserted } from "./checkEnoughMoneyWasInserted";
 import { checkDrinkShortage } from "./checkDrinkShortage";
-import { Drink } from "./type";
+import { Dependencies, Drink } from "./type";
 import { beverageQuantityChecker } from "../dependencies/beverageQuantityChecker";
 import { emailNotifier } from "../dependencies/emailNotifier";
 import { buildDependencies } from "../dependencies/dependencies";
+import { error, MappingFunction, Maybe, success } from "../utils/monad";
 
 export type NumberOfSugars = 0 | 1 | 2
 export type StickNeed = "with_stick" | "without_stick";
@@ -45,7 +46,7 @@ export type ErrorMessage =
 const dependencies = buildDependencies();
 
 
-export const orderDrink = (userOrder: UserOrder<Drink>, moneyInserted: MoneyInserted): MachineOrder<Drink> | ErrorMessage => {
+/*export const orderDrink = (userOrder: UserOrder<Drink>, moneyInserted: MoneyInserted): MachineOrder<Drink> | ErrorMessage => {
   const isDrinkEmpty = checkDrinkShortage(dependencies)(userOrder.drink);
   if (isDrinkEmpty) {
     return {
@@ -72,4 +73,42 @@ export const orderDrink = (userOrder: UserOrder<Drink>, moneyInserted: MoneyInse
     numberOfSugars: userOrder.numberOfSugars,
     stickNeed: "with_stick"
   }
+}*/
+
+const handleDrinkShortage = (dependencies: Dependencies) => (userOrder: UserOrder<Drink>): Maybe<UserOrder<Drink>> => {
+  const isDrinkEmpty = checkDrinkShortage(dependencies)(userOrder.drink);
+  if (isDrinkEmpty) {
+    return error(`Too bad. Shortage of ${userOrder.drink}. The company has been notified.`)
+  }
+  return success(userOrder)
+}
+
+const handleEnoughMoneyWasInserted = (moneyInserted: MoneyInserted) => (userOrder: UserOrder<Drink>): Maybe<UserOrder<Drink>> => {
+  const checkMoneyResult = checkEnoughMoneyWasInserted(userOrder.drink, moneyInserted)
+  if (checkMoneyResult.checkMoneyResult === "not_enough") {
+    return error(`Missing ${checkMoneyResult.missingMoney}€ to complete order`)
+  }
+  return success(userOrder);
+}
+
+const makeDrink = (userOrder: UserOrder<Drink>): Maybe<MachineOrder<Drink>> => {
+  if (userOrder.numberOfSugars === 0) {
+    return success({
+      ...userOrder,
+      numberOfSugars: 0,
+      stickNeed: "without_stick",
+    })
+  }
+  return success({
+    ...userOrder,
+    numberOfSugars: userOrder.numberOfSugars,
+    stickNeed: "with_stick"
+  })
+}
+
+export const orderDrink = (userOrder: UserOrder<Drink>, moneyInserted: MoneyInserted): Maybe<MachineOrder<Drink>> => {
+  return success(userOrder)
+    .bind(handleDrinkShortage(dependencies))
+    .bind(handleEnoughMoneyWasInserted(moneyInserted))
+    .bind(makeDrink)
 }
